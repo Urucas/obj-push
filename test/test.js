d59410d359b5fa36052071d786f765189e2206c1
vrunoa <vruno.alassia@gmail.com>
2015-07-27 00:51:03 -0300

    add multiple keys

--- test/test.js
+++ test/test.js
@@ -2,14 +2,25 @@ import push from '../lib/';
 
 describe("obj-push instance test", () => {
 
-  it("should add an object", (done) => {
+  it("should push an object with one key", (done) => {
     let foo = {'foo':'bar'};
     let bar = {'bar':'foo'};
 
     foo = push(foo, bar);
-    if(foo.bar != bar)
+    if(foo.bar != bar.bar)
       throw new Error("Fail to add object, "+JSON.stringify(foo));
     done();
   });
+
+  it("should push an object with multiple keys", (done) => {
+    let foo = {'foo':'bar'};
+    let bar = {'bar':'foo','foo':'foo','bor':'off'};
     
+    foo = push(foo, bar);
+    if(foo.bar != bar.bar || foo.foo != bar.foo || foo.bor != bar.bor)
+      throw new Error("Fail to add object, "+JSON.stringify(foo));
+    done();
+  });
+
+   
 });
